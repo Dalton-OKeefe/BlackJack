a809c04eeaeb21653d78d2dfa2984289d7627baa
Dalton-OKeefe <113914645+Dalton-OKeefe@users.noreply.github.com>
2024-11-04 22:40:11 -0500

    xml & main activity
    set up initial buttons in xml and starting to code functional code in the main activity.

---
+++ app/src/main/java/com/example/blackjack/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.blackjack;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.GridLayout;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+import java.util.Collections;
+public class MainActivity {
+}
